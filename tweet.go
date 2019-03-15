d1331c5d866186d4f85c7901dbc4b6613350bc44
Anharu <haruan2394@gmail.com>
2019-03-15 23:56:47 +0900

    Add: go script for tweet

---
+++ tweet.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"./request"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func getTwitterApi() *anaconda.TwitterApi {
+	anaconda.SetConsumerKey(os.Getenv("TWITTER_KEY"))
+	anaconda.SetConsumerSecret(os.Getenv("TWITTER_SECRET"))
+	return anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+}
+
+func main() {
+
+	loadEnv()
+
+	api := getTwitterApi()
+
+	result_posts := request.GetTrendPosts()
+	fmt.Printf("%v", result_posts[0].Title)
+
+	log.Printf("Hello")
+}
